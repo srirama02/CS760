7f195a139211cba32d431c8fa4d9167c5f027b93
Sriram Ashokkumar <ashokkumar2@wisc.edu>
2023-11-08 21:27:44 -0600

    update

--- HW5/1.2.py
+++ HW5/1.2.py
@@ -1,164 +1,133 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
-from scipy.optimize import linear_sum_assignment
 
-# Function to compute the clustering objective for K-means
-def kmeans_objective(X, labels, centroids):
-    distances = np.linalg.norm(X - centroids[labels])
-    return np.sum(distances**2)
+def calculate_kmeans_objective(data_points, cluster_labels, cluster_centroids):
+    point_to_centroid_distances = np.linalg.norm(data_points - cluster_centroids[cluster_labels], axis=1)
+    return np.sum(point_to_centroid_distances**2)
 
-def evl_acc(means, centroids, labels, true_labels):
-    # Compute the accuracy of the clustering
-    # First map each centroid to a cluster based on the true means
-    mapping = {}
-    for i in range(len(centroids)):
-        min_dist = np.inf
-        for j in range(len(means)):
-            dist = np.linalg.norm(centroids[i] - means[j])
-            if dist < min_dist:
-                min_dist = dist
-                mapping[i] = j
-    # Convert all the labels based on this mapping
-    for i in range(len(labels)):
-        labels[i] = mapping[labels[i]]
- 
-    # Compute the accuracy
-    acc = 0
-    for i in range(len(labels)):
-        if labels[i] == true_labels[i]:
-            acc += 1
- 
-    return acc / len(labels)
+def evaluate_clustering_accuracy(ideal_means, determined_centroids, assigned_labels, true_cluster_labels):
+    centroid_to_mean_mapping = {}
+    for centroid_index in range(len(determined_centroids)):
+        minimum_distance = np.inf
+        for mean_index in range(len(ideal_means)):
+            distance_to_mean = np.linalg.norm(determined_centroids[centroid_index] - ideal_means[mean_index])
+            if distance_to_mean < minimum_distance:
+                minimum_distance = distance_to_mean
+                centroid_to_mean_mapping[centroid_index] = mean_index
+    for label_index in range(len(assigned_labels)):
+        assigned_labels[label_index] = centroid_to_mean_mapping[assigned_labels[label_index]]
 
-# # Function to compute the clustering objective for GMM
-# def gmm_objective(X, labels, means, covariances):
-#     total_log_likelihood = 0
-#     for k in range(len(means)):
-#         cluster_points = X[labels == k]
-#         if len(cluster_points) > 0:
-#             log_likelihood = multivariate_normal.logpdf(cluster_points, mean=means[k], cov=covariances[k])
-#             total_log_likelihood += np.sum(log_likelihood)
-#     return total_log_likelihood
+    accurate_count = sum(assigned_label == true_label for assigned_label, true_label in zip(assigned_labels, true_cluster_labels))
+    return accurate_count / len(assigned_labels)
 
-def gmm_negative_log_likelihood(X, labels, means, covariances):
-    total_negative_log_likelihood = 0
-    for k in range(len(means)):
-        cluster_points = X[labels == k]
-        if len(cluster_points) > 0:
-            log_likelihood = multivariate_normal.logpdf(cluster_points, mean=means[k], cov=covariances[k])
-            total_negative_log_likelihood -= np.sum(log_likelihood)  # Summing negative log likelihood
-    return total_negative_log_likelihood
+def compute_gmm_negative_log_likelihood(data_points, gmm_labels, gmm_means, gmm_covariances):
+    neg_log_likelihood_total = 0
+    for mean_index, mean in enumerate(gmm_means):
+        data_in_cluster = data_points[gmm_labels == mean_index]
+        if data_in_cluster.size > 0:
+            cluster_log_likelihood = multivariate_normal.logpdf(data_in_cluster, mean=mean, cov=gmm_covariances[mean_index])
+            neg_log_likelihood_total -= np.sum(cluster_log_likelihood)
+    return neg_log_likelihood_total
 
-# K-means algorithm
-def kmeans(X, K, max_iters=100):
-    # Initialization step: pick random samples as initial centroids
-    centroids = X[np.random.choice(X.shape[0], K, replace=False)]
-    for i in range(max_iters):
-        # Assignment step: assign each point to the closest centroid
-        # labels = np.argmin(np.linalg.norm(X[:, np.newaxis] - centroids, axis=2), axis=1)
-        # new_centroids = np.array([X[labels == k].mean(axis=0) for k in range(K)])
-        distances = np.linalg.norm(X[:, None] - centroids[None, :], axis=2)
-        labels = np.argmin(distances, axis=1)
-        new_centroids = np.array([X[labels == k].mean(axis=0) for k in range(K)])
+def perform_kmeans_clustering(data_points, num_clusters, max_iterations=100):
+    centroids = data_points[np.random.choice(data_points.shape[0], num_clusters, replace=False)]
+    for iteration in range(max_iterations):
+        distances_to_centroids = np.linalg.norm(data_points[:, np.newaxis] - centroids[np.newaxis, :], axis=2)
+        closest_centroid_labels = np.argmin(distances_to_centroids, axis=1)
+        updated_centroids = np.array([data_points[closest_centroid_labels == k].mean(axis=0) for k in range(num_clusters)])
 
-        # Check for convergence
-        if np.all(new_centroids == centroids):
+        if np.all(updated_centroids == centroids):
             break
-        centroids = new_centroids
-    return labels, centroids
+        centroids = updated_centroids
+    return closest_centroid_labels, centroids
 
-# GMM algorithm
-def gmm(X, K, max_iters=100, tol=1e-3):
-    n, d = X.shape
-    means = X[np.random.choice(n, K, replace=False)]
-    covariances = [np.eye(d) * np.var(X, axis=0) for _ in range(K)]
-    pis = np.ones(K) / K
-    r = np.zeros((n, K))
-    prev_log_likelihood = -np.inf
+def perform_gmm_clustering(data_points, num_clusters, max_iterations=100, convergence_tolerance=1e-3):
+    num_points, num_dimensions = data_points.shape
+    gmm_means = data_points[np.random.choice(num_points, num_clusters, replace=False)]
+    gmm_covariances = [np.eye(num_dimensions) * np.var(data_points, axis=0) for _ in range(num_clusters)]
+    mixing_coefficients = np.ones(num_clusters) / num_clusters
+    responsibilities = np.zeros((num_points, num_clusters))
+    previous_log_likelihood = -np.inf
 
-    for iteration in range(max_iters):
-        # E-step: compute responsibilities
-        for k in range(K):
-            r[:, k] = pis[k] * multivariate_normal.pdf(X, mean=means[k], cov=covariances[k])
+    for iteration in range(max_iterations):
+        for cluster_index in range(num_clusters):
+            responsibilities[:, cluster_index] = mixing_coefficients[cluster_index] * multivariate_normal.pdf(data_points, mean=gmm_means[cluster_index], cov=gmm_covariances[cluster_index])
 
-        log_likelihood = np.sum(np.log(np.dot(r, pis)))
+        log_likelihood = np.sum(np.log(np.dot(responsibilities, mixing_coefficients)))
         
-        # Check for convergence
-        if np.abs(log_likelihood - prev_log_likelihood) <= tol:
+        if np.abs(log_likelihood - previous_log_likelihood) <= convergence_tolerance:
             break
-        prev_log_likelihood = log_likelihood
+        previous_log_likelihood = log_likelihood
 
-        r /= r.sum(axis=1, keepdims=True)
+        responsibilities /= responsibilities.sum(axis=1, keepdims=True)
 
-        # M-step: update parameters
-        N_k = r.sum(axis=0)
-        for k in range(K):
-            means[k] = np.dot(r[:, k], X) / N_k[k]
-            X_centered = X - means[k]
-            covariances[k] = np.dot(r[:, k] * X_centered.T, X_centered) / N_k[k]
-            pis[k] = N_k[k] / n
+        cluster_sizes = responsibilities.sum(axis=0)
+        for cluster_index in range(num_clusters):
+            gmm_means[cluster_index] = np.dot(responsibilities[:, cluster_index], data_points) / cluster_sizes[cluster_index]
+            data_points_centered = data_points - gmm_means[cluster_index]
+            gmm_covariances[cluster_index] = np.dot(responsibilities[:, cluster_index] * data_points_centered.T, data_points_centered) / cluster_sizes[cluster_index]
+            mixing_coefficients[cluster_index] = cluster_sizes[cluster_index] / num_points
 
-    # Assign points to clusters
-    labels = np.argmax(r, axis=1)
-    return labels, means, covariances
+    final_labels = np.argmax(responsibilities, axis=1)
+    return final_labels, gmm_means, gmm_covariances
 
-
-# Generate the synthetic data
-def generate_data(sigma):
-    n = 100  # number of points per cluster
-    P_a = np.random.multivariate_normal([-1, -1], [[2*sigma, 0.5*sigma], [0.5*sigma, 1*sigma]], n)
-    P_b = np.random.multivariate_normal([1, -1], [[1*sigma, -0.5*sigma], [-0.5*sigma, 2*sigma]], n)
-    P_c = np.random.multivariate_normal([0, 1], [[1*sigma, 0], [0, 2*sigma]], n)
-    X = np.concatenate((P_a, P_b, P_c), axis=0)
-    true_labels = np.array([0]*n + [1]*n + [2]*n)
-    true_means = [[-1,-1], [1,-1], [0,1]]
-    return X, true_labels, true_means
-
-# Values of sigma for which to generate data and perform clustering
-sigmas = [0.5, 1, 2, 4, 8]
-
-# Arrays to hold the objective and accuracy values for each sigma
-kmeans_objectives = []
-kmeans_accuracies = []
-gmm_objectives = []
-gmm_accuracies = []
-
-# Main loop over sigma values
-for sigma in sigmas:
-    # Generate data
-    X, true_labels, true_means = generate_data(sigma)
+def generate_synthetic_data(variability_factor):
+    samples_per_cluster = 100
+    cluster_A_mean = [-1, -1]
+    cluster_B_mean = [1, -1]
+    cluster_C_mean = [0, 1]
+    cluster_A_covariance = [[2*variability_factor, 0.5*variability_factor], [0.5*variability_factor, 1*variability_factor]]
+    cluster_B_covariance = [[1*variability_factor, -0.5*variability_factor], [-0.5*variability_factor, 2*variability_factor]]
+    cluster_C_covariance = [[1*variability_factor, 0], [0, 2*variability_factor]]
     
-    # Perform K-means clustering
-    kmeans_labels, kmeans_centroids = kmeans(X, K=3)
-    kmeans_objectives.append(kmeans_objective(X, kmeans_labels, kmeans_centroids))
-    kmeans_accuracies.append(evl_acc(true_means, kmeans_centroids, kmeans_labels, true_labels))
+    points_A = np.random.multivariate_normal(cluster_A_mean, cluster_A_covariance, samples_per_cluster)
+    points_B = np.random.multivariate_normal(cluster_B_mean, cluster_B_covariance, samples_per_cluster)
+    points_C = np.random.multivariate_normal(cluster_C_mean, cluster_C_covariance, samples_per_cluster)
     
-    # Perform GMM clustering
-    gmm_labels, gmm_means, gmm_covariances = gmm(X, K=3)
-    gmm_objectives.append(gmm_negative_log_likelihood(X, gmm_labels, gmm_means, gmm_covariances))
-    gmm_accuracies.append(evl_acc(true_means, gmm_means, gmm_labels, true_labels))
+    combined_points = np.concatenate((points_A, points_B, points_C), axis=0)
+    actual_labels = np.array([0]*samples_per_cluster + [1]*samples_per_cluster + [2]*samples_per_cluster)
+    true_centroid_means = [cluster_A_mean, cluster_B_mean, cluster_C_mean]
+    
+    return combined_points, actual_labels, true_centroid_means
 
-    # gmm_accuracies.append(clustering_accuracy(true_labels, gmm_labels, K=3))
+variability_factors = [0.5, 1, 2, 4, 8]
 
-# Plotting the results
-fig, axs = plt.subplots(1, 2, figsize=(14, 6))
+kmeans_objective_values = []
+kmeans_accuracy_scores = []
+gmm_objective_values = []
+gmm_accuracy_scores = []
 
-# Plot clustering objectives
-axs[0].plot(sigmas, kmeans_objectives, marker='o', label='K-means Objective')
-axs[0].plot(sigmas, gmm_objectives, marker='x', label='GMM Objective')
-axs[0].set_xlabel('Sigma')
-axs[0].set_ylabel('Clustering Objective')
-axs[0].set_title('Clustering Objective vs Sigma')
-axs[0].legend()
+for variability in variability_factors:
+    synthetic_data, actual_labels, ideal_means = generate_synthetic_data(variability)
+    
+    kmeans_assigned_labels, kmeans_determined_centroids = perform_kmeans_clustering(synthetic_data, num_clusters=3)
+    kmeans_objective_values.append(calculate_kmeans_objective(synthetic_data, kmeans_assigned_labels, kmeans_determined_centroids))
+    kmeans_accuracy_scores.append(evaluate_clustering_accuracy(ideal_means, kmeans_determined_centroids, kmeans_assigned_labels, actual_labels))
+    
+    gmm_assigned_labels, gmm_determined_means, gmm_determined_covariances = perform_gmm_clustering(synthetic_data, num_clusters=3)
+    gmm_objective_values.append(compute_gmm_negative_log_likelihood(synthetic_data, gmm_assigned_labels, gmm_determined_means, gmm_determined_covariances))
+    gmm_accuracy_scores.append(evaluate_clustering_accuracy(ideal_means, gmm_determined_means, gmm_assigned_labels, actual_labels))
 
-# Plot clustering accuracies
-axs[1].plot(sigmas, kmeans_accuracies, marker='o', label='K-means Accuracy')
-axs[1].plot(sigmas, gmm_accuracies, marker='x', label='GMM Accuracy')
-axs[1].set_xlabel('Sigma')
-axs[1].set_ylabel('Clustering Accuracy')
-axs[1].set_title('Clustering Accuracy vs Sigma')
-axs[1].legend()
+
+
+
+fig, axis = plt.subplots(1, 2, figsize=(14, 6))
+
+axis[0].plot(variability_factors, kmeans_objective_values, marker='o', label='K-means Objective')
+axis[0].plot(variability_factors, gmm_objective_values, marker='x', label='GMM Objective')
+axis[0].set_xlabel('Variability Factor (Sigma)')
+axis[0].set_ylabel('Clustering Objective Value')
+axis[0].set_title('Clustering Objective vs Variability Factor')
+axis[0].legend()
+
+axis[1].plot(variability_factors, kmeans_accuracy_scores, marker='o', label='K-means Accuracy')
+axis[1].plot(variability_factors, gmm_accuracy_scores, marker='x', label='GMM Accuracy')
+axis[1].set_xlabel('Variability Factor (Sigma)')
+axis[1].set_ylabel('Clustering Accuracy Score')
+axis[1].set_title('Clustering Accuracy vs Variability Factor')
+axis[1].legend()
 
 plt.tight_layout()
+plt.savefig("1.2.png")
 plt.show()
--- HW5/2.3.py
+++ HW5/2.3.py
@@ -1,22 +1,18 @@
 import numpy as np
 import pandas as pd
 from numpy.linalg import svd
-from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
 
-# Load the data from a CSV file
 def load_data(file_name):
     return pd.read_csv(file_name, header=None).values
 
-# Buggy PCA
 def buggy_pca(X, d):
     U, S, VT = svd(X, full_matrices=False)
     Z = np.dot(X, VT.T[:, :d])
     reconstruction = np.dot(Z, VT[:d, :])
     return Z, VT, reconstruction
 
-# Demeaned PCA
 def demeaned_pca(X, d):
     mean_X = np.mean(X, axis=0)
     demeaned_X = X - mean_X
@@ -24,7 +20,6 @@ def demeaned_pca(X, d):
     reconstruction += mean_X
     return Z, VT, reconstruction
 
-# Normalized PCA
 def normalized_pca(X, d):
     mean_X = np.mean(X, axis=0)
     std_X = np.std(X, axis=0)
@@ -33,140 +28,98 @@ def normalized_pca(X, d):
     reconstruction = reconstruction * std_X + mean_X
     return Z, VT, reconstruction
 
-# DRO Implementation
-def dro(X, d):
-    n, D = X.shape
-    one_n = np.ones((n, 1))
+def dro(data, d):
+    num_samples, original_dimension = data.shape
+    
+    offset = np.mean(data, axis=0)
+    centered_data = data - offset
+    
+    U, S, Vt = np.linalg.svd(centered_data, full_matrices=False)
+    
+    U_reduced = U[:, :d]
+    singular_values_reduced = S[:d]
+    Vt_reduced = Vt[:d, :]
+    A_transposed = np.dot(np.diag(singular_values_reduced), Vt_reduced)
+    reduced_representations = U_reduced
+    reconstructions = np.dot(reduced_representations, A_transposed) + offset
+    
+    return reduced_representations, A_transposed, U_reduced, reconstructions, np.diag(singular_values_reduced)
 
-    # Objective function for DRO
-    def objective(params):
-        A = params[:D * d].reshape(D, d)
-        b = params[D * d:D * d + D]
-        Z = params[D * d + D:].reshape(n, d)
-        return np.linalg.norm(X - Z @ A.T - one_n @ b.reshape(1, D), 'fro')**2 / n
-
-    # Initial guess for A, b, and Z
-    A_init = np.random.rand(D, d)
-    b_init = np.random.rand(D)
-    Z_init = np.random.rand(n, d)
-    params_init = np.concatenate([A_init.ravel(), b_init, Z_init.ravel()])
-
-    # Minimize the objective function
-    result = minimize(objective, params_init, method='L-BFGS-B')
-
-    # Extract the optimal values for A, b, and Z
-    A_opt = result.x[:D * d].reshape(D, d)
-    b_opt = result.x[D * d:D * d + D]
-    Z_opt = result.x[D * d + D:].reshape(n, d)
-    reconstruction = Z_opt @ A_opt.T + one_n @ b_opt.reshape(1, D)
-    return Z_opt, (A_opt, b_opt), reconstruction
-
-# Reconstruction error calculation
 def reconstruction_error(X, reconstructed_X):
     return np.sum((X - reconstructed_X)**2)
 
-# Load datasets
 data2D = load_data('data/data2D.csv')
 data1000D = load_data('data/data1000D.csv')
 
-# Choose 'd' for the 1000D dataset
-# For the sake of this example, let's choose d=3, but you should choose it based on the singular values analysis.
-chosen_d = 3
+chosen_d = 31
 
-# Apply methods on 2D dataset
 Z_buggy, _, rec_buggy = buggy_pca(data2D, d=1)
 Z_demeaned, _, rec_demeaned = demeaned_pca(data2D, d=1)
 Z_normalized, _, rec_normalized = normalized_pca(data2D, d=1)
-Z_dro, params_dro, rec_dro = dro(data2D, d=1)
+d_dim_representations, At, Z, rec_dro, S_diag = dro(data2D, 1)
 
-# Calculate reconstruction errors for 2D dataset
 error_buggy = reconstruction_error(data2D, rec_buggy)
 error_demeaned = reconstruction_error(data2D, rec_demeaned)
 error_normalized = reconstruction_error(data2D, rec_normalized)
 error_dro = reconstruction_error(data2D, rec_dro)
 
-# Apply methods on 1000D dataset
+U, S, VT = svd(data1000D, full_matrices=False)
+
+plt.figure(figsize=(10, 5))
+plt.plot(S, 'o-', markersize=4, label='Singular Values')
+plt.title('Singular Value Spectrum (1000D Dataset)')
+plt.xlabel('Index')
+plt.ylabel('Singular Value')
+plt.yscale('log')
+plt.grid(True)
+plt.legend()
+plt.savefig("2.3.kneePoint.png")
+
+
 Z_buggy_1000D, _, rec_buggy_1000D = buggy_pca(data1000D, chosen_d)
 Z_demeaned_1000D, _, rec_demeaned_1000D = demeaned_pca(data1000D, chosen_d)
 Z_normalized_1000D, _, rec_normalized_1000D = normalized_pca(data1000D, chosen_d)
-Z_dro_1000D, params_dro_1000D, rec_dro_1000D = dro(data1000D, chosen_d)
+d_dim_representations, At, Z, rec_dro_1000D, S_diag = dro(data1000D, chosen_d)
 
-# Calculate reconstruction errors for 1000D dataset
 error_buggy_1000D = reconstruction_error(data1000D, rec_buggy_1000D)
 error_demeaned_1000D = reconstruction_error(data1000D, rec_demeaned_1000D)
 error_normalized_1000D = reconstruction_error(data1000D, rec_normalized_1000D)
 error_dro_1000D = reconstruction_error(data1000D, rec_dro_1000D)
 
-# Print reconstruction errors
+print(f"Reconstruction error for Buggy PCA (2D): {error_buggy}")
+print(f"Reconstruction error for Demeaned PCA (2D): {error_demeaned}")
+print(f"Reconstruction error for Normalized PCA (2D): {error_normalized}")
+print(f"Reconstruction error for DRO (2D): {error_dro}")
+
 print(f"Reconstruction error for Buggy PCA (1000D): {error_buggy_1000D}")
 print(f"Reconstruction error for Demeaned PCA (1000D): {error_demeaned_1000D}")
 print(f"Reconstruction error for Normalized PCA (1000D): {error_normalized_1000D}")
 print(f"Reconstruction error for DRO (1000D): {error_dro_1000D}")
-# Applying DRO on the 2D dataset again
-Z_dro, params_dro, rec_dro = dro(data2D, chosen_d)
 
-# Visualization for 2D dataset including DRO
-fig, axs = plt.subplots(2, 3, figsize=(18, 12))  # Adjusted for an additional method
+fig, axs = plt.subplots(2, 2, figsize=(18, 12))
 
-# Original points
 axs[0, 0].scatter(data2D[:, 0], data2D[:, 1], color='blue', label='Original Data')
-axs[0, 0].set_title('Original Data')
+axs[0, 0].scatter(rec_buggy[:, 0], rec_buggy[:, 1], color='red', label='Buggy PCA Reconstruction')
+axs[0, 0].set_title('Buggy PCA Reconstruction')
 
-# Buggy PCA reconstruction
 axs[0, 1].scatter(data2D[:, 0], data2D[:, 1], color='blue', label='Original Data')
-axs[0, 1].scatter(rec_buggy[:, 0], rec_buggy[:, 1], color='red', label='Buggy PCA Reconstruction')
-axs[0, 1].set_title('Buggy PCA Reconstruction')
+axs[0, 1].scatter(rec_demeaned[:, 0], rec_demeaned[:, 1], color='green', label='Demeaned PCA Reconstruction')
+axs[0, 1].set_title('Demeaned PCA Reconstruction')
 
-# Demeaned PCA reconstruction
-axs[0, 2].scatter(data2D[:, 0], data2D[:, 1], color='blue', label='Original Data')
-axs[0, 2].scatter(rec_demeaned[:, 0], rec_demeaned[:, 1], color='green', label='Demeaned PCA Reconstruction')
-axs[0, 2].set_title('Demeaned PCA Reconstruction')
-
-# Normalized PCA reconstruction
 axs[1, 0].scatter(data2D[:, 0], data2D[:, 1], color='blue', label='Original Data')
 axs[1, 0].scatter(rec_normalized[:, 0], rec_normalized[:, 1], color='purple', label='Normalized PCA Reconstruction')
 axs[1, 0].set_title('Normalized PCA Reconstruction')
 
-# DRO reconstruction
 axs[1, 1].scatter(data2D[:, 0], data2D[:, 1], color='blue', label='Original Data')
 axs[1, 1].scatter(rec_dro[:, 0], rec_dro[:, 1], color='orange', label='DRO Reconstruction')
 axs[1, 1].set_title('DRO Reconstruction')
 
-# Hide the last subplot (unused)
-axs[1, 2].axis('off')
-
-# Set labels and titles for each subplot
 for ax in axs.flat:
-    if not ax.axis()[0] == 0.0:  # Only set labels for non-hidden subplots
+    if not ax.axis()[0] == 0.0:
         ax.set(xlabel='X-axis', ylabel='Y-axis')
         ax.label_outer()
         ax.legend()
 
-# Adjust layout to prevent overlapping
 plt.tight_layout()
+plt.savefig("2.3.png")
 plt.show()
-# # Visualization for 2D dataset
-# fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-
-# # Original points
-# axs[0, 0].scatter(data2D[:, 0], data2D[:, 1], color='blue', label='Original Data')
-# axs[0, 0].set_title('Original Data')
-
-# # Buggy PCA reconstruction
-# axs[0, 1].scatter(rec_buggy[:, 0], rec_buggy[:, 1], color='red', label='Buggy PCA Reconstruction')
-# axs[0, 1].set_title('Buggy PCA Reconstruction')
-
-# # Demeaned PCA reconstruction
-# axs[1, 0].scatter(rec_demeaned[:, 0], rec_demeaned[:, 1], color='green', label='Demeaned PCA Reconstruction')
-# axs[1, 0].set_title('Demeaned PCA Reconstruction')
-
-# # Normalized PCA reconstruction
-# axs[1, 1].scatter(rec_normalized[:, 0], rec_normalized[:, 1], color='purple', label='Normalized PCA Reconstruction')
-# axs[1, 1].set_title('Normalized PCA Reconstruction')
-
-# for ax in axs.flat:
-#     ax.set(xlabel='X-axis', ylabel='Y-axis')
-#     ax.label_outer()
-
-# plt.legend()
-# plt.show()
